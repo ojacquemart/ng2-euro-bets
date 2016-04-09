75b7c43db8269ea3dcedbe5a1b53285ed35d3843
Olivier Jacquemart <o.jacquemart@gmail.com>
2016-04-09 07:09:49 +0200

    style(user): add user avatar component

---
+++ src/app/core/components/user-avatar/user-avatar.component.ts
@@ -0,0 +1,19 @@
+import {Component} from 'angular2/core';
+import {Auth} from "../../services/firebase/auth.service";
+
+@Component({
+  selector: 'pronos-user-avatar',
+  styles: [require('./user-avatar.scss')],
+  template: require('./user-avatar.html')
+})
+export class UserAvatar {
+
+  private user:FirebaseAuthDataGoogle = null;
+
+  constructor(private auth: Auth) {}
+
+  ngOnInit() {
+    this.user = this.auth.user;
+  }
+
+}
