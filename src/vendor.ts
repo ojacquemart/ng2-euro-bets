3b28d382bcc9a97637b314c5044c0b83ef0bba3a
Olivier Jacquemart <o.jacquemart@gmail.com>
2016-04-14 07:19:04 +0200

    chore: add global rx import

--- src/vendor.ts
+++ src/vendor.ts
@@ -14,6 +14,7 @@ import 'angular2/router';
 // RxJS
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/mergeMap';
+import 'rxjs/Rx';
 
 // Angular 2 Material 2
 // TODO(gdi2290): uncomment when material is fixed
