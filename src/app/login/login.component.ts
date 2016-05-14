45d48e33bb4500d6239bfd8f0f31f280fdfb3896
Olivier Jacquemart <o.jacquemart@gmail.com>
2016-05-14 22:50:02 +0200

    fea(i18n): i18n login page
    Don't use ng2 translate to avoid blinking effect

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -5,6 +5,12 @@ import {Auth} from '../core/services/firebase/auth.service';
 import {UefaEuroLogoCmp} from '../core/components/uefa-euro-logo/uefa-euro-logo.component';
 
 import {LOGIN_PROVIDERS, Provider} from './provider.model';
+import {UserLang} from "../core/services/util/user-lang.service";
+
+const SIGN_IN_LABELS = {
+  fr:  'Se connecter',
+  en: 'Sign in'
+};
 
 @Component({
   selector: 'bets-login',
@@ -15,11 +21,13 @@ import {LOGIN_PROVIDERS, Provider} from './provider.model';
 export class LoginCmp {
 
   private providers: Array<Provider>;
+  private signInLabel;
 
   constructor(private auth:Auth) {
     console.log('login @ init');
 
     this.providers = LOGIN_PROVIDERS;
+    this.signInLabel = SIGN_IN_LABELS[UserLang.getLang()];
   }
 
   ngOnInit() {
