455b1a81ce5f8f2e8891de5ae76a03f796816123
Olivier Jacquemart <o.jacquemart@gmail.com>
2016-06-27 22:49:24 +0200

    polish(bets): hide tobets tab when no bets available

--- src/app/bets/matches/matches.component.ts
+++ src/app/bets/matches/matches.component.ts
@@ -1,7 +1,5 @@
 import {Component} from 'angular2/core';
 
-import {Observable} from 'rxjs/Observable';
-
 import {BetsCardListCmp} from '../card-list/card-list.component';
 import {MatchesService} from '../services/matches.service';
 import {MatchGroup, Matches} from '../models/bets.models';
@@ -13,9 +11,11 @@ import {MatchGroup, Matches} from '../models/bets.models';
 })
 export class MatchesBetsCmp {
 
-  private current: Array<MatchGroup>;
-  private history: Array<MatchGroup>;
-  private showingHistory: boolean;
+  private current:Array<MatchGroup>;
+  private history:Array<MatchGroup>;
+
+  private hidingCurrentMatches:boolean;
+  private showingHistory:boolean;
 
   constructor(private matches:MatchesService) {
     console.log('match bets @ init');
@@ -26,6 +26,9 @@ export class MatchesBetsCmp {
       .subscribe((matches:Matches) => {
         this.current = matches.current;
         this.history = matches.history;
+
+        this.hidingCurrentMatches = this.current.length === 0;
+        this.showingHistory = this.hidingCurrentMatches;
       });
   }
 
